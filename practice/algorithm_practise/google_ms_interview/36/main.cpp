d34204bdf193a6105f43d4b319cd9abebcdd9138
yushiyaogg <yushiyaogg@163.com>
2014-02-23 18:07:08 +0800

    36

--- practice/algorithm_practise/google_ms_interview/36/main.cpp
+++ practice/algorithm_practise/google_ms_interview/36/main.cpp
@@ -1,2 +1,43 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
+
+int Delete_chars(char* OriginalString, char* DeleteCharacter)
+{
+    int hash[256] = {0};
+    char *scanptr = NULL;
+    char *modifyptr = NULL;
+    if(OriginalString == NULL||DeleteCharacter == NULL)
+        return -1;
+    for(int i = 0; i < sizeof(DeleteCharacter) -1; i ++)
+    {
+        hash[*(DeleteCharacter + i)] = 1;
+    }
+
+    scanptr = modifyptr = OriginalString;
+    while(*scanptr != '\0')
+    {
+        if(hash[*(scanptr)] == 0)
+        {
+            memcpy(modifyptr,scanptr,1);//should use memcpy
+            scanptr ++;
+            modifyptr ++;
+        }
+        else
+        {
+            //*(modifyptr) = *(scanptr+1); char* can not use "="
+            scanptr ++;
+        }
+    }
+    memcpy(modifyptr,scanptr,1);
+    return 0;
+
+}
+
+int main()
+{
+    char string1[] = "we are the champion. u are the loser.";
+    char D_char[] = "aeiou";
+    Delete_chars(string1,D_char);
+    cout << string1<<endl;
+}
